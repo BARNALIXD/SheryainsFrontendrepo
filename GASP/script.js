ee5c5cb67a7cdf682625ffaa89a5e2eaddb3eba4
BARNALIDAS <dasbarnali690@gcekbpatna.ac.in>
2024-06-23 15:06:33 +0000

    .

---
+++ GASP/script.js
@@ -0,0 +1,65 @@
+// gsap.to('#box', {
+//     x:1000,
+//     rotate:360 ,
+//     duration :2,
+//     delay:1,
+//     backgroundColor : "blue",
+// }) 
+
+// gsap.from('#box', {
+//     x:1000,
+//     rotate:360 ,
+//     duration :2,
+//     delay:1,
+//     backgroundColor : "blue",
+// })
+
+
+ 
+// gsap.to ("#box1", {
+//     x:1200,
+//     rotate:360,
+//     scale:0.5,
+//     duration:2,
+//     delay:1,
+// })
+// gsap.to ("#box2", {
+//     x:1200,
+//     rotate:360,
+//     scale:0.5,
+//     duration:2,
+//     delay:3,
+// }) 
+// gsap.to ("box3", {
+//     x:1200,
+//     rotate:360,
+//     scale:0.5,
+//     duration:2,
+//     delay:3,
+// })
+
+
+/* GSAP TIMELINE */
+
+// var tl = gsap.timeline()
+// tl.to ("#box1", {
+//     x:1200,
+//     rotate:360,
+//     scale:0.5,
+//     duration:2,
+//     delay:3
+// })
+// tl.to ("#box2", {
+//     x:1200,
+//     rotate:360,
+//     scale:0.5,
+//     duration:2,
+//     delay:3
+// })
+// tl.to ("#box3", {
+//     x:1200,
+//     rotate:360,
+//     scale:0.5,
+//     duration:2,
+//     delay:3
+// })
